50d692a7cb57481d691d15a8c6923714e388da52
huyquyentran <45568163+huyquyentran@users.noreply.github.com>
2020-10-24 18:15:53 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+curl https://raw.githubusercontent.com/huyquyentran/quantrimang/main/qdns.sh --output /usr/local/bin/qdns
+curl https://raw.githubusercontent.com/huyquyentran/quantrimang/main/qdns.conf --output /etc/qdns.conf
+
+echo " =========================================================="
+echo "||                  INSTALL SUCCESSFULLY                  ||"
+echo "||       Please reopen this terminal to use my script     ||"
+echo " =========================================================="
