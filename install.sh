d373df3db3080ecb5df05322e39641bd46304ef4
huyquyentran <45568163+huyquyentran@users.noreply.github.com>
2020-10-24 18:19:37 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 curl https://raw.githubusercontent.com/huyquyentran/quantrimang/main/qdns.sh --output /usr/local/bin/qdns
 curl https://raw.githubusercontent.com/huyquyentran/quantrimang/main/qdns.conf --output /etc/qdns.conf
 
+chmod 755 /usr/local/bin/qdns
+chmod +x /usr/local/bin/qdns
+
 echo " =========================================================="
 echo "||                  INSTALL SUCCESSFULLY                  ||"
 echo "||       Please reopen this terminal to use my script     ||"
